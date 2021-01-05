fe4b652c0ab9a6f44d1bf84468e87bb3f7f9b0a9
Anushek Prasal <anushekprasal@gmail.com>
2021-01-05 15:19:43 +0530

    xdroid: soong: Add camera_needs_client_info
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: I60a9b2a137d70e7592b43dd2f9a05bbb6a2ef694
    (cherry picked from commit 5fcb5d558342aca3cd83a61530cb375ad3349048)

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += xdroidGlobalVars
 SOONG_CONFIG_xdroidGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     ignores_ftp_pptp_conntrack_failure \
@@ -62,6 +63,7 @@ SOONG_CONFIG_xdroidQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_xdroidGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_xdroidGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_xdroidGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_xdroidGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
